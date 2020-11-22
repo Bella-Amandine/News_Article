f74d51a0a8381fba516e892cfcc58d9e0d53992e
Bella Amandine <bellamandy45@gmail.com>
2020-11-22 18:33:23 +0200

    Define source class to holds data from source api

---
+++ app/models/source.py
@@ -0,0 +1,10 @@
+class Source:
+    '''
+    Source class  to define Source Objects
+    '''
+
+    def __init__(self, id, name, description, category):
+        self.source_id = id
+        self.source_name = name
+        self.source_description = description
+        self.category = category
